10521812b79503e3b45c8b73e7f690ef225f9af7
Kezia Boyce <105072417+kboyce25@users.noreply.github.com>
2022-05-27 03:33:27 +0000

    h

--- part5.py
+++ part5.py
@@ -3,12 +3,11 @@
 #start writing your code below
 month = input("Enter a month:")
 
-if month == ("april","june","november","september"):
+if month == ("april"or"june"or"november"or"september"):
   print("30")
-if month == ("january","february", "march", "may", "july", "august", "october", "december"):
+if month == ("january"or"february"or "march"or "may", "july"or "august"or "october"or "december"):
   print("31")
-else:
-    print("not a month")
+
   
   
 
